5388e7c148ca09b259e944724196c4b74d4fc1b9
Mike G <followthroughworks@gmail.com>
2016-11-18 13:56:26 -0500

    Added projects and work to the dashboard template and view

--- punchclock/views.py
+++ punchclock/views.py
@@ -5,5 +5,8 @@ from .models import Puncher
 def dashboard(request):
     user = Puncher.objects.get(pk=1)
     username = user.__str__()
-    context = {'username': username}
+
+    all_projects = list(user.projects_shared.all())
+
+    context = {'username': username, 'all_projects': all_projects}
     return render(request, 'dashboard.html', context)
