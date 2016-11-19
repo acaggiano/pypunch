eee94cda379215719070af1088eee77ac91ce8b6
Mike G <followthroughworks@gmail.com>
2016-11-19 01:48:28 -0500

    Added most recent punch on user projects

--- punchclock/views.py
+++ punchclock/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
-from .models import Puncher
+from .models import Puncher, Work
 
 def dashboard(request):
     user = Puncher.objects.get(pk=1)
     username = user.__str__()
     most_recent_punch = user.work_set.all().latest('time_start')
+    project_recent_punch = Work.objects.filter(
+        project__creator=user
+    ).latest('time_start')
 
     all_projects = list(user.projects_shared.all())
 
-    context = {'username': username, 'most_recent_punch': most_recent_punch, 'all_projects': all_projects}
+    context = {'username': username, 'most_recent_punch': most_recent_punch,
+               'project_recent_punch': project_recent_punch, 'all_projects': all_projects}
     return render(request, 'dashboard.html', context)
