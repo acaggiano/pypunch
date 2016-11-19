4559751215116dfc727b7dd83cb3699803bd1d58
Mike G <followthroughworks@gmail.com>
2016-11-19 01:41:37 -0500

    Added most recent punch

--- punchclock/views.py
+++ punchclock/views.py
@@ -5,8 +5,9 @@ from .models import Puncher
 def dashboard(request):
     user = Puncher.objects.get(pk=1)
     username = user.__str__()
+    most_recent_punch = user.work_set.all().latest('time_start')
 
     all_projects = list(user.projects_shared.all())
 
-    context = {'username': username, 'all_projects': all_projects}
+    context = {'username': username, 'most_recent_punch': most_recent_punch, 'all_projects': all_projects}
     return render(request, 'dashboard.html', context)
