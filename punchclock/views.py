19260bd4a456ee46eca3ad5b08d5b067f5afec02
Mike G <followthroughworks@gmail.com>
2016-11-19 02:07:19 -0500

    Added most recent project shared with user

--- punchclock/views.py
+++ punchclock/views.py
@@ -6,12 +6,12 @@ def dashboard(request):
     user = Puncher.objects.get(pk=1)
     username = user.__str__()
     most_recent_punch = user.work_set.all().latest('time_start')
-    project_recent_punch = Work.objects.filter(
-        project__creator=user
-    ).latest('time_start')
+    project_recent_punch = Work.objects.filter(project__creator=user).latest('time_start')
+    most_recent_share = user.projects_shared.exclude(creator=user).latest('created')
 
     all_projects = list(user.projects_shared.all())
 
     context = {'user':user, 'username': username, 'most_recent_punch': most_recent_punch,
-               'project_recent_punch': project_recent_punch, 'all_projects': all_projects}
+               'project_recent_punch': project_recent_punch, 'most_recent_share': most_recent_share,
+               'all_projects': all_projects}
     return render(request, 'dashboard.html', context)
