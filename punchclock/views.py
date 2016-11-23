812c398120b849760b1e9dbba98d70778976ca43
Caggiano <Caggiano>
2016-11-23 16:48:39 -0500

    fixed queries in dashboard view

--- punchclock/views.py
+++ punchclock/views.py
@@ -11,15 +11,25 @@ def index(request):
 def dashboard(request):
     user = request.user
     username = user.__str__()
-    #most_recent_punch = user.profile.work_set.all().latest('time_start')
-    #project_recent_punch = Work.objects.filter(project__creator=user.profile).latest('time_start')
-    #most_recent_share = user.profile.projects_shared.exclude(creator=user.profile).latest('created')
+    try:
+        most_recent_punch = user.profile.work_set.all().latest('time_start')
+    except Work.DoesNotExist:
+        most_recent_punch = None
+
+    try:
+        project_recent_punch = Work.objects.filter(project__creator=user.profile).latest('time_start')
+    except Work.DoesNotExist:
+        project_recent_punch = None
+    try:
+        most_recent_share = user.profile.projects_shared.exclude(creator=user.profile).latest('created')
+    except Project.DoesNotExist:
+        most_recent_share = None
     
     all_projects = Project.objects.filter(creator=request.user.profile).order_by('created')
 
-    context = {'user':user, 'username': username, #'most_recent_punch': most_recent_punch,
-               #'project_recent_punch': project_recent_punch, 
-               #'most_recent_share': most_recent_share,
-               #'all_projects': all_projects
+    context = {'user':user, 'username': username, 'most_recent_punch': most_recent_punch,
+               'project_recent_punch': project_recent_punch, 
+               'most_recent_share': most_recent_share,
+               'all_projects': all_projects
                }
     return render(request, 'punchclock/dashboard.html', context)
